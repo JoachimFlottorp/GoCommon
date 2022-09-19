149fb676e5df6b52ece7c663d021f10b6236a1d5
JoachimF <joachim@flottorp.org>
2022-09-20 00:46:17 +0200

    JSON Helpers (#5)
    * feat: (De)Serialize helpers
    
    * Tests for SerializeStruct

---
+++ helper/json/json.go
@@ -0,0 +1,20 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// DeserializeStruct: creates a struct from io.Reader interface
+func DeserializeStruct[T interface{}](reader io.Reader) (*T, error) {
+	var C T
+
+	err := json.NewDecoder(reader).Decode(&C)
+	
+	return &C, err
+}
+
+// SerializeStruct: Creates a []byte from a struct
+func SerializeStruct[T interface{}](c T) ([]byte, error) {
+	return json.Marshal(c)
+}
---
+++ helper/json/json_test.go
@@ -0,0 +1,77 @@
+package json
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeserializeStruct(t *testing.T) {
+	type T struct {
+		A string `json:"a"`
+		B string `json:"b"`
+	}
+
+	type TestCase struct {
+		Input      string
+		Expected   T
+		ShouldFail bool
+	}
+
+	testCases := []TestCase{
+		{
+			Input:      "{\"a\": \"foo\", \"b\": \"bar\"}",
+			Expected:   T{A: "foo", B: "bar"},
+			ShouldFail: false,
+		},
+		{
+			Input:      "{\"c\": \"foo\", \"b\": \"bar\"",
+			Expected:   T{A: "foo", B: "bar"},
+			ShouldFail: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		reader := strings.NewReader(testCase.Input)
+		result, err := DeserializeStruct[T](reader)
+		if testCase.ShouldFail {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.Expected, *result)
+		}
+	}
+
+}
+
+func TestSerializeStruct(t *testing.T) {
+	type T struct {
+		A string `json:"a"`
+		B string `json:"b"`
+	}
+
+	type TestCase struct {
+		Input      T
+		Expected   string
+		ShouldFail bool
+	}
+
+	testCases := []TestCase{
+		{
+			Input:      T{A: "foo", B: "bar"},
+			Expected:   "{\"a\":\"foo\",\"b\":\"bar\"}",
+			ShouldFail: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		result, err := SerializeStruct(testCase.Input)
+		if testCase.ShouldFail {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.Expected, string(result))
+		}
+	}
+}
